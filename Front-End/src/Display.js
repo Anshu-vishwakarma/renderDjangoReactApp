84b6b83268c5d76a5c690129989d1bc4de4e67ec
Anshu Kumar vishwakarma <89008910+Anshu-vishwakarma@users.noreply.github.com>
2024-07-08 16:41:17 +0530

    Update Display.js

--- Front-End/src/Display.js
+++ Front-End/src/Display.js
@@ -8,9 +8,9 @@ const Display=()=>{
 
 const [myData, setdata]= useState([]);
 
-const loadData=()=>{
-       let apis = `http://127.0.0.1:8000/Employ/`
-              axios.get(apis).then((res)=>{
+const loadData=async()=>{
+       let apis = `https://renderdjangoreactapp.onrender.com/Employ/`
+             await axios.get(apis).then((res)=>{
                      setdata(res.data);
               });
 }
